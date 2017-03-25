e108cd4c28091c364523aca7dd59286e6cf1b0b6
PK <pratush87@gmail.com>
2017-03-25 18:47:42 -0500

    first commit

---
+++ src/main/java/com/shc/ecom/sywrelay/reporting/config/BootConfig.java
@@ -0,0 +1,11 @@
+package com.shc.ecom.sywrelay.reporting.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootConfig {
+	public static void main(String[] args) {
+		SpringApplication.run(BootConfig.class, args);
+	}
+}
